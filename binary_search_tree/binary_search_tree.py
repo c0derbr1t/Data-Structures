1efeecc1bb2f6e65550d5fe18cd54225ff028f6c
c0derbr1t <c0der.br1t@gmail.com>
2020-07-16 15:15:21 -0500

    Fix imports

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,5 @@
+from stack.stack import Stack
+from queue.queue import Queue
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
